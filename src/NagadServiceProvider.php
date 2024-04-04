ab1363ef0133a5fc07edb95a6847251d4b4d4156
Jeishanul Haque Shishir <razinsoft.shishir@gmail.com>
2024-04-04 15:00:38 +0600

    version 1.0.1

--- src/NagadServiceProvider.php
+++ src/NagadServiceProvider.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace Jeishanul\Nagad;
+
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Foundation\AliasLoader;
+
 /**
  * Service Provider class
  * @author Jeishanul Haque Shishir <shishirjeishanul@gmail.com>
@@ -10,29 +12,30 @@ use Illuminate\Foundation\AliasLoader;
 
 class NagadServiceProvider extends ServiceProvider
 {
-  /**
-   * Bootstrap any application services.
-   *
-   * @return void
-   */
-   public function boot()
-   {
-       $this->publishes([
-        __DIR__ . '/../config/nagad.php' => config_path('nagad.php'),
-      ], 'config');
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/nagad.php' => config_path('nagad.php'),
+        ], 'config');
 
-       AliasLoader::getInstance()->alias('NagadPayment', 'Jeishanul\Nagad\Facades\Nagad');
-   }
+        AliasLoader::getInstance()->alias('NagadPayment', 'Jeishanul\Nagad\Facades\Nagad');
+    }
 
-  /**
-   * Register any application services.
-   *
-   * @return void
-   */
-   public function register()
-   {
-       $this->app->bind('nagad', function () {
-           return new Nagad;
-       });
-   }
+    
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('nagad', function () {
+            return new Nagad;
+        });
+    }
 }
